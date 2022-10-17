da6b46c57e8dab8e60de6c273a2dc2f1c0df137c
danielgalc <daniel.gallego@iesdonana.org>
2022-10-17 19:29:14 +0200

    Validación Insertar Codigo/Denom

--- insertar.php
+++ insertar.php
@@ -15,8 +15,48 @@
 
     $codigo = obtener_post('codigo');
     $denominacion = obtener_post('denominacion');
+    $error = [];
 
     if (isset($codigo, $denominacion)) {
+        // Validacion código:
+        $long = mb_strlen($codigo);
+        if ($long >= 1 && $long <= 2){
+            // Todos los caracteres son dígitos:
+            if(ctype_digit($codigo)){
+                // El código no está repetido:
+                $pdo = conectar();
+                $sent = $pdo->prepare("SELECT COUNT(*) FROM departamentos WHERE codigo = :codigo");
+                $sent->execute([':codigo' => $codigo]);
+                $cuantos = $sent->fetchColumn();
+                if($cuantos === 0){
+                    // El código es válido
+
+                } else {
+                    $error[] = 'El código ya existe.';
+                }
+            } else {
+                $error[] = 'Los caracteres no son válidos';
+            }
+        } else {
+            $error[] = 'La longitud del código es incorrecta';
+        }
+
+        // Validación denominación:
+
+        $long = mb_strlen($denominacion);
+        if ($long >= 1 && $long <= 255){
+            // La denominación es válida.
+        } else {
+            $error[] = 'La longitud de la denominación es incorrecta';
+        }
+
+        $pdo = conectar();
+        $sent = $pdo->prepare("INSERT INTO departamentos (codigo, denominacion) VALUES (:codigo, :denominacion)");
+        $sent->execute([':codigo' => $codigo, ':denominacion' => $denominacion]);
+        return volver();
+    }
+
+    if (empty($error)){
         $pdo = conectar();
         $sent = $pdo->prepare("INSERT INTO departamentos (codigo, denominacion) VALUES (:codigo, :denominacion)");
         $sent->execute([':codigo' => $codigo, ':denominacion' => $denominacion]);
