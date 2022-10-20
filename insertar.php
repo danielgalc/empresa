bed69f3ff684b5ddbac737b13de1e2d06eb5bd31
danielgalc <daniel.gallego@iesdonana.org>
2022-10-20 19:28:27 +0200

    Validaciones en Auxiliar

--- insertar.php
+++ insertar.php
@@ -1,78 +1,46 @@
 <!DOCTYPE html>
 <html lang="es">
-
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Insertar un nuevo departamento</title>
 </head>
-
 <body>
     <?php
-
     require 'auxiliar.php';
 
     $codigo = obtener_post('codigo');
     $denominacion = obtener_post('denominacion');
+
     $error = [];
 
     if (isset($codigo, $denominacion)) {
-        // Validacion código:
-        $long = mb_strlen($codigo);
-        if ($long >= 1 && $long <= 2){
-            // Todos los caracteres son dígitos:
-            if(ctype_digit($codigo)){
-                // El código no está repetido:
-                $pdo = conectar();
-                $sent = $pdo->prepare("SELECT COUNT(*) FROM departamentos WHERE codigo = :codigo");
-                $sent->execute([':codigo' => $codigo]);
-                $cuantos = $sent->fetchColumn();
-                if($cuantos === 0){
-                    // El código es válido
+        filtrar_codigo($codigo, $error);
+        filtrar_denominacion($denominacion, $error);
 
-                } else {
-                    $error[] = 'El código ya existe.';
-                }
-            } else {
-                $error[] = 'Los caracteres no son válidos';
-            }
+        if (!empty($error)) {
+            mostrar_errores($error);
         } else {
-            $error[] = 'La longitud del código es incorrecta';
-        }
-
-        // Validación denominación:
-
-        $long = mb_strlen($denominacion);
-        if ($long >= 1 && $long <= 255){
-            // La denominación es válida.
-        } else {
-            $error[] = 'La longitud de la denominación es incorrecta';
-        }
-        
-        
-        if (empty($error)){
-            $pdo = conectar();
-            $sent = $pdo->prepare("INSERT INTO departamentos (codigo, denominacion) VALUES (:codigo, :denominacion)");
-            $sent->execute([':codigo' => $codigo, ':denominacion' => $denominacion]);
+            insertar_departamento($codigo, $denominacion);
             return volver();
         }
     }
-
-
     ?>
     <div>
         <form action="" method="post">
             <div>
                 <label>
                     Código:
-                    <input type="text" name="codigo">
+                    <input type="text" name="codigo" size="10"
+                           value="<?= $codigo ?>">
                 </label>
             </div>
             <div>
                 <label>
                     Denominación:
-                    <input type="text" name="denominacion">
+                    <input type="text" name="denominacion"
+                           value="<?= $denominacion ?>">
                 </label>
             </div>
             <div>
@@ -82,5 +50,4 @@
         </form>
     </div>
 </body>
-
 </html>
