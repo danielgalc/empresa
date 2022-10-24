c4e2f464f62e9ee9c07bc5ddd9ee10dc940aa456
danielgalc <daniel.gallego@iesdonana>
2022-10-24 13:21:50 +0200

    Abstracciones Auxiliar + Insertar + Comienzo Modificar v2

--- borrar.php
+++ borrar.php
@@ -4,7 +4,6 @@ require 'auxiliar.php';
 
 $id = obtener_post('id');
 
-<<<<<<< HEAD
 if (!isset($id)) {
     return volver();
 }
@@ -15,13 +14,3 @@ $pdo = conectar();
 $sent = $pdo->prepare("DELETE FROM departamentos WHERE id = :id");
 $sent->execute([':id' => $id]);
 volver();
-=======
-if (!isset($id)){
-    return volver();
-}
-
-$pdo = conectar();
-$sent = $pdo->prepare('DELETE FROM departamentos WHERE id = :id');
-$sent->execute([':id' => $id]);
-return volver();
->>>>>>> 91c1eeac01def8ef69624df24efa7d5e9349b769
