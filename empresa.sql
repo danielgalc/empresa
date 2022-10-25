1d193bac43101678dacadcf823d0dd973fae7bcc
danielgalc <daniel.gallego@iesdonana.org>
2022-10-25 20:09:48 +0200

    Creamos la tabla EMPLEADOS

--- empresa.sql
+++ empresa.sql
@@ -7,6 +7,17 @@ CREATE TABLE departamentos (
     denominacion varchar(255) NOT NULL
 );
 
+DROP TABLE IF EXISTS empleados CASCADE;
+
+CREATE TABLE empleados (
+    id              bigserial         PRIMARY KEY,
+    numero          numeric(4)        NOT NULL UNIQUE,
+    nombre          varchar(255)      NOT NULL,
+    salario         numeric(7,2)      NOT NULL,
+    fecha_nac       timestamp NOT NULL,
+    departamento_id bigint            NOT NULL REFERENCES departamentos(id)
+);
+
 -- Fixtures:
 
 INSERT INTO departamentos (codigo, denominacion)
@@ -15,3 +26,9 @@ INSERT INTO departamentos (codigo, denominacion)
            (30, 'Prevención'),
            (40, 'Laboratorio'),
            (50, 'Automoción');
+
+INSERT INTO empleados (numero, nombre, salario, fecha_nac, departamento_id)
+     VALUES (1, 'Pepe', 1500, '1999-01-01', 1),
+            (4, 'Manolo', 1800, '1999-03-02', 2),
+            (5, 'Juan', 1400, '1999-04-05', 3),
+            (9, 'Antonio', 1900, '1999-10-12', 4);
